785cd1e5320edfcba7362277564bfbabf630ff3b
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-16 09:22:24 +0200

    add static files and prepare static_root folder use for production

--- forumLocsApp/forumLocsApp/settings.py
+++ forumLocsApp/forumLocsApp/settings.py
@@ -120,13 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'djangobower.finders.BowerFinder',
 )
+
 
 # Directory where bower package are installed
 BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "static/")
