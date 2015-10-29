ff61210c2a0a75cfc19c2ceb57c0a033da2568a3
caille_j <caille_j@epitech.eu>
2015-10-29 16:29:55 +0000

    modified init_porject

--- init_project.sh
+++ init_project.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 if [ -f $HOME/.virtualenvs/forumLocsapp/bin/activate ]
 then
     source $HOME/.virtualenvs/forumLocsapp/bin/activate
@@ -13,15 +12,12 @@ npm install
 ./manage.py bower_install
 gulp css
 cd -
+forumLocsApp/manage.py flush << 'EOF'
+yes
+EOF
 forumLocsApp/manage.py loaddata create_user
 forumLocsApp/manage.py loaddata create_categories
 forumLocsApp/manage.py init_forum
-echo "Now just run workon forumLocsapp and ./forumLocsApp/manage.py runserver"
-=======
-source /home/sylflo/.virtualenvs/locsAppForum/bin/activate
 forumLocsApp/manage.py makemigrations
 forumLocsApp/manage.py migrate
-forumLocsApp/manage.py loaddata create_user
-forumLocsApp/manage.py loaddata create_categories
-forumLocsApp/manage.py init_forum
->>>>>>> 35ea65b30ea2ce71466b63f435590153c73e7148
+echo "Now just run workon forumLocsapp and ./forumLocsApp/manage.py runserver"
