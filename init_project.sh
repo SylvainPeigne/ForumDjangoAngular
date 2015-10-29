c4909e0d0ab46a72aa3e5be34763107d5f3cc542
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-29 17:07:32 +0800

    remove call

--- init_project.sh
+++ init_project.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 source /home/sylflo/.virtualenvs/locsAppForum/bin/activate
+forumLocsApp/manage.py makemigrations
+forumLocsApp/manage.py migrate
 forumLocsApp/manage.py loaddata create_user
 forumLocsApp/manage.py loaddata create_categories
 forumLocsApp/manage.py init_forum
