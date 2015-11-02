24882956337af02dc72ea084b71676ed3f0ed42a
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-02 18:35:11 +0800

    creating model for userForum

--- forumLocsApp/authentication/models.py
+++ forumLocsApp/authentication/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserForum(models.Model):
+    User = models.ForeignKey(default=None, null=True)
