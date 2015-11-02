2896f8297ad8e355998a38f3a7f1dc34e56f219f
caille_j <caille_j@epitech.eu>
2015-11-02 17:42:32 +0000

    inflated UserForum model and added default picture

--- forumLocsApp/authentication/models.py
+++ forumLocsApp/authentication/models.py
@@ -5,3 +5,9 @@ from django.contrib.auth.models import User
 
 class UserForum(models.Model):
     User = models.ForeignKey(default=None, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_last_connected = models.DateTimeField(auto_add=True)
+    messages_written = models.IntegerField()
+    url_avatar = models.URLField(default="/media/user_avatars/default/default.jpg")
+    score = models.IntegerField()
+    role = models.IntegerField(default=0)
