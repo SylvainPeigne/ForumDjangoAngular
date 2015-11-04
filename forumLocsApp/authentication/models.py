4bce3e0abc0940eed29cfb6249a26175ae1db7bd
caille_j <caille_j@epitech.eu>
2015-11-04 10:48:49 +0000

    added fixture to create extended user, changed UserForum model

--- forumLocsApp/authentication/models.py
+++ forumLocsApp/authentication/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 
 class UserForum(models.Model):
     User = models.ForeignKey(User, default=None, null=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    date_last_connected = models.DateTimeField(auto_now=True)
-    messages_written = models.IntegerField()
-    url_avatar = models.URLField(default="/media/user_avatars/default/default.jpg")
-    score = models.IntegerField()
-    role = models.IntegerField(default=0)
+    date_joined = models.DateTimeField(default=datetime.datetime.now, null=True)
+    date_last_connected = models.DateTimeField(auto_now=True, null=True)
+    messages_written = models.IntegerField(default=0, null=True)
+    url_avatar = models.URLField(default="/media/user_avatars/default/default.jpg", null=True)
+    score = models.IntegerField(null=True)
+    role = models.IntegerField(default=0, null=True)
